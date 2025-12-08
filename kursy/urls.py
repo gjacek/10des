2a9d507559003eb727cf6b470a28236d8930d755
Jacek <jacek.gajc@gmail.com>
2025-12-08 15:49:21 +0100

    Update authentication UI, templates and settings

--- kursy/urls.py
+++ kursy/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
-from . import api_views  # Uncommented to enable API endpoints
+# from . import api_views  # TODO: Uncomment when DRF is installed
 
 urlpatterns = [
     # Frontend views
     path('', views.login_view, name='login'),
     path('begin/', views.begin, name='begin'),
     
-    # API endpoints
-    path('api/auth/login/', api_views.login_view_api, name='api_login'),
-    path('api/auth/register/', api_views.register_view_api, name='api_register'),
+    # API endpoints - TODO: Uncomment when DRF is installed
+    # path('api/auth/login/', api_views.login_view_api, name='api_login'),
+    # path('api/auth/register/', api_views.register_view_api, name='api_register'),
 ]
